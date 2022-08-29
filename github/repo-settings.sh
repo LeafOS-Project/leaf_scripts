80b3b09f1e03528bfba0d30bd9ec64323a5face5
Tim Zimmermann <tim@linux4.de>
2022-08-29 14:42:44 +0200

    scripts: github: Create missing repos
    Change-Id: Ib660d2e1c56da861d0a928803a0f5b2e152e60fc

--- github/repo-settings.sh
+++ github/repo-settings.sh
@@ -8,9 +8,17 @@ fi
 LEAF_VERSION=$(cat .repo/manifests/snippets/leaf.xml  | grep -i '<default revision' | cut -f2 -d '"' | cut -f3 -d '/')
 
 for REPO in $(cat .repo/manifests/snippets/leaf.xml | grep -E 'LeafOS-Project|LeafOS-Blobs' | cut -f4 -d '"'); do
+	echo $REPO
+	curl -X POST \
+	-H "Accept: application/vnd.github+json" \
+	-H "Authorization: token $GH_TOKEN" \
+	https://api.github.com/orgs/$(echo $REPO | cut -f1 -d '/')/repos \
+	-d "{\"name\":\"$(echo $REPO | cut -f2 -d '/')\",\"private\":false,\"has_issues\":false,\"has_projects\":false,\"has_wiki\":false}" \
+	--silent > /dev/null
 	curl -X PATCH \
 	-H "Accept: application/vnd.github+json" \
 	-H "Authorization: token $GH_TOKEN" \
         https://api.github.com/repos/$REPO \
-	-d "{\"has_issues\":false,\"has_projects\":false,\"has_wiki\":false,\"default_branch\":\"$LEAF_VERSION\"}"
+	-d "{\"has_issues\":false,\"has_projects\":false,\"has_wiki\":false,\"default_branch\":\"$LEAF_VERSION\"}" \
+	--silent > /dev/null
 done
