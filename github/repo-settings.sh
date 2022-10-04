ff0a213eb164693af0f5862de0ad68cf68f59db9
SamarV-121 <samarvispute121@pm.me>
2022-10-04 09:25:34 +0000

    scripts: github: Create gerrit project as well
     * also reformat script
    
    Change-Id: I3192baaa7c6fc10cadfc63c905c268541a52c7a1

--- github/repo-settings.sh
+++ github/repo-settings.sh
@@ -5,20 +5,40 @@ if [ -z "$GH_TOKEN" ]; then
 	exit 1
 fi
 
-LEAF_VERSION=$(cat .repo/manifests/snippets/leaf.xml  | grep -i '<default revision' | cut -f2 -d '"' | cut -f3 -d '/')
+PORT="29418"
+GERRIT="review.leafos.org"
+GERRIT_PROJECTS=($(ssh -p "$PORT" "$GERRIT" gerrit ls-projects))
+LEAF_VERSION=$(grep -i '<default revision' .repo/manifests/snippets/leaf.xml | cut -f2 -d '"' | cut -f3 -d '/')
 
-for REPO in $(cat .repo/manifests/snippets/leaf.xml | grep -E 'LeafOS-Project|LeafOS-Blobs|LeafOS-Devices' | cut -f4 -d '"'); do
-	echo $REPO
+grep -E 'LeafOS-Project|LeafOS-Blobs|LeafOS-Devices' .repo/manifests/snippets/leaf.xml | cut -f4 -d '"' | while IFS= read -r PROJECT; do
+	ORG=$(echo "$PROJECT" | cut -f1 -d '/')
+	REPO=$(echo "$PROJECT" | cut -f2 -d '/')
+
+	echo "$PROJECT"
 	curl -X POST \
 	-H "Accept: application/vnd.github+json" \
 	-H "Authorization: token $GH_TOKEN" \
-	https://api.github.com/orgs/$(echo $REPO | cut -f1 -d '/')/repos \
-	-d "{\"name\":\"$(echo $REPO | cut -f2 -d '/')\",\"private\":false,\"has_issues\":false,\"has_projects\":false,\"has_wiki\":false}" \
+	"https://api.github.com/orgs/$ORG/repos" \
+	-d "{
+		\"name\":\"$REPO\",
+		\"private\":false,
+		\"has_issues\":false,
+		\"has_projects\":false,
+		\"has_wiki\":false
+	}" \
 	--silent > /dev/null
+
 	curl -X PATCH \
 	-H "Accept: application/vnd.github+json" \
 	-H "Authorization: token $GH_TOKEN" \
-        https://api.github.com/repos/$REPO \
-	-d "{\"has_issues\":false,\"has_projects\":false,\"has_wiki\":false,\"default_branch\":\"$LEAF_VERSION\"}" \
+        "https://api.github.com/repos/$PROJECT" \
+	-d "{
+		\"has_issues\":false,
+		\"has_projects\":false,
+		\"has_wiki\":false,
+		\"default_branch\":\"$LEAF_VERSION\"
+	}" \
 	--silent > /dev/null
+
+	[[ "$GERRIT_PROJECTS" =~ "$PROJECT" ]] && ssh -p "$PORT" "$GERRIT" gerrit create-project "$PROJECT"
 done
