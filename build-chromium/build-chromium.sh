51a2baa5dc00cdea91e7250b1d4e98b68f32c073
Tim Zimmermann <tim@linux4.de>
2022-12-17 06:41:56 +0100

    scripts: build-chromium: Initial commit
    * Script based on https://github.com/LineageOS/scripts/blob/45d62060bfaca101f52b14d0e61b295b6921fa40/build-webview/build-webview.sh
    * Patches taken from https://github.com/GrapheneOS/Vanadium/tree/422333246c7cd40cec02aac751ed8d3637c652e9/patches
    
    Change-Id: Id0df182c99d2cf2c372af12f0f93be74b8908756

---
+++ build-chromium/build-chromium.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+set -e
+
+chromium_version="108.0.5359.79"
+chromium_code="5359079"
+clean=0
+gsync=0
+supported_archs=(arm64 x64)
+
+webview_proj_base=android_external_chromium-webview
+
+usage() {
+    echo "Usage:"
+    echo "  build_webview [ options ]"
+    echo
+    echo "  Options:"
+    echo "    -a <arch> Build specified arch"
+    echo "    -c Clean"
+    echo "    -h Show this message"
+    echo "    -r <release> Specify chromium release"
+    echo "    -s Sync"
+    echo
+    echo "  Example:"
+    echo "    build_webview -c -r $chromium_version:$chromium_code"
+    echo
+    exit 1
+}
+
+build() {
+    build_args=$args' target_cpu="'$1'"'
+
+    code=$chromium_code
+    if [ $1 '==' "arm" ]; then
+        code+=00
+    elif [ $1 '==' "arm64" ]; then
+        code+=50
+    elif [ $1 '==' "x86" ]; then
+        code+=10
+    elif [ $1 '==' "x64" ]; then
+        code+=60
+    fi
+    build_args+=' android_default_version_code="'$code'"'
+
+    gn gen "out/$1" --args="$build_args"
+    ninja -C out/$1 trichrome_webview_64_apk trichrome_chrome_64_apk trichrome_library_64_apk
+}
+
+while getopts ":a:chr:s" opt; do
+    case $opt in
+        a) for arch in ${supported_archs[@]}; do
+               [ "$OPTARG" '==' "$arch" ] && build_arch="$OPTARG"
+           done
+           if [ -z "$build_arch" ]; then
+               echo "Unsupported ARCH: $OPTARG"
+               echo "Supported ARCHs: ${supported_archs[@]}"
+               exit 1
+           fi
+           ;;
+        c) clean=1 ;;
+        h) usage ;;
+        r) version=(${OPTARG//:/ })
+           chromium_version=${version[0]}
+           chromium_code=${version[1]}
+           ;;
+        s) gsync=1 ;;
+        :)
+          echo "Option -$OPTARG requires an argument"
+          echo
+          usage
+          ;;
+        \?)
+          echo "Invalid option:-$OPTARG"
+          echo
+          usage
+          ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# Add depot_tools to PATH
+if [ ! -d depot_tools ]; then
+    git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+fi
+export PATH="$(pwd -P)/depot_tools:$PATH"
+
+if [ ! -d src ]; then
+    fetch --no-history android
+    yes | gclient sync -D -R -r $chromium_version --no-history
+fi
+
+# Apply our patches
+if [ $gsync -eq 1 ]; then
+    ( cd src
+      git am $(realpath $(dirname $0))/patches/*.patch
+    )
+fi
+
+if [ $gsync -eq 1 ]; then
+    find src -name index.lock -delete
+    yes | gclient sync -R -r $chromium_version --no-history
+fi
+cd src
+
+# Replace webview icon
+cp chrome/android/java/res_chromium_base/mipmap-mdpi/app_icon.png android_webview/nonembedded/java/res_icon/drawable-mdpi/icon_webview.png
+cp chrome/android/java/res_chromium_base/mipmap-hdpi/app_icon.png android_webview/nonembedded/java/res_icon/drawable-hdpi/icon_webview.png
+cp chrome/android/java/res_chromium_base/mipmap-xhdpi/app_icon.png android_webview/nonembedded/java/res_icon/drawable-xhdpi/icon_webview.png
+cp chrome/android/java/res_chromium_base/mipmap-xxhdpi/app_icon.png android_webview/nonembedded/java/res_icon/drawable-xxhdpi/icon_webview.png
+
+# Build args
+args='target_os="android"'
+args+=' is_debug=false'
+args+=' is_official_build=true'
+args+=' is_chrome_branded=false'
+args+=' use_official_google_api_keys=false'
+args+=' ffmpeg_branding="Chrome"'
+args+=' proprietary_codecs=true'
+args+=' enable_resource_allowlist_generation=false'
+args+=' enable_remoting=false'
+args+=' is_component_build=false'
+args+=' symbol_level=0'
+args+=' enable_nacl=false'
+args+=' blink_symbol_level=0'
+args+=' webview_devui_show_icon=false'
+args+=' dfmify_dev_ui=false'
+args+=' disable_autofill_assistant_dfm=true'
+args+=' disable_tab_ui_dfm=true'
+args+=' enable_gvr_services=false'
+args+=' disable_fieldtrial_testing_config=true'
+args+=' android_default_version_name="'$chromium_version'"'
+args+=' chrome_public_manifest_package="org.leafos.chromium"'
+args+=' system_webview_package_name="org.leafos.webview"'
+args+=' trichrome_library_package="org.leafos.trichromelibrary"'
+
+# Setup environment
+[ $clean -eq 1 ] && rm -rf out
+. build/android/envsetup.sh
+
+# Check target and build
+if [ -n "$build_arch" ]; then
+    build $build_arch
+else
+    build arm64
+    build x64
+fi
