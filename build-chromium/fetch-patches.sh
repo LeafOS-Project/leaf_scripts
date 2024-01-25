a29bf09cfad3f85c06d709ad9b738400157f0287
Tim Zimmermann <tim@linux4.de>
2024-01-25 06:46:16 +0100

    scripts: build-chromium: Update to 121.0.6167.71
    * Patches from https://github.com/GrapheneOS/Vanadium/tree/d40275b1366e1693b06b7ea722bf65dfa4e82945/patches
    
    Change-Id: If839da150386fde1217ae641a9d42141aef59354

---
+++ build-chromium/fetch-patches.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PATCHES_UPSTREAM=$(curl --silent https://github.com/GrapheneOS/Vanadium/tree/main/patches | jq)
+
+for PATCH in patches/*.patch; do
+    PATCH=$(basename $PATCH)
+    NUM=$(echo $PATCH | cut -f1 -d '-')
+    PATCHNAME=$(echo $PATCH | sed -e "s/^$NUM-//g")
+    NEWNAME=$(echo "$PATCHES_UPSTREAM" | grep -e "\"[0-9]*-$PATCHNAME\"")
+    NEWNAME=$(echo "$NEWNAME" | cut -f4 -d '"')
+    if [ -z "$NEWNAME" ]; then
+        echo "Missing patch $PATCH"
+        exit 1
+    fi
+
+     echo $NEWNAME
+    mv patches/$PATCH patches/$NEWNAME
+    wget -O patches/$NEWNAME https://raw.githubusercontent.com/GrapheneOS/Vanadium/main/patches/$NEWNAME
+done
